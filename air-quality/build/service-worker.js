ff37110ee15d903648ef6cba94a7144e20c7d571
Artem Larin <xperial2609@gmail.com>
2020-09-28 21:45:25 +0300

    deploy

--- air-quality/build/service-worker.js
+++ air-quality/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.668930af2be4c3af2a8c716a0cdbabaa.js"
+  "/WEB_bootcamp/precache-manifest.23ba84df69e5655c0b0966f1cda5e3d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WEB_bootcamp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
